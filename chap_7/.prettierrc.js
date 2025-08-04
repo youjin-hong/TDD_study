e7bc7e287fe9a25ccd3b9dbac8cacf8dc2381d54
youjin-hong <asdfghjk1052@naver.com>
2025-08-04 21:02:11 +0900

    refactor: jest에서 vitest로 마이그레이션

---
+++ chap_7/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  tailingComma: "all",
+  printWidth: 100,
+};
--- chap_7/vite.config.ts
+++ chap_7/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+
 export default defineConfig({
   plugins: [react()],
   test: {
